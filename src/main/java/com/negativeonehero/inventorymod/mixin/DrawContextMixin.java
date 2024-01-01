26e30d881f27847b46e9f64d1af33ba9d66bcc6f
Nguyen Phi Hung <notahero2004@gmail.com>
2024-01-01 08:57:48 +0700

    0.9.0: Changes
    - DrawContextMixin.draw() will only parse non-empty strings.

--- src/main/java/com/negativeonehero/inventorymod/mixin/DrawContextMixin.java
+++ src/main/java/com/negativeonehero/inventorymod/mixin/DrawContextMixin.java
@@ -3,9 +3,11 @@ package com.negativeonehero.inventorymod.mixin;
 import com.negativeonehero.inventorymod.InventoryMod;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import org.apache.commons.lang3.StringUtils;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -15,7 +17,9 @@ import java.text.DecimalFormat;
 import java.util.Objects;
 
 @Mixin(DrawContext.class)
-public class DrawContextMixin {
+public abstract class DrawContextMixin {
+
+    @Shadow public abstract int drawText(TextRenderer textRenderer, Text text, int x, int y, int color, boolean shadow);
 
     @Unique
     private String formatDouble(double number, int decimalPlaces) {
@@ -27,28 +31,28 @@ public class DrawContextMixin {
 
     @Redirect(method = "drawItemInSlot(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/item/ItemStack;IILjava/lang/String;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawText(Lnet/minecraft/client/font/TextRenderer;Ljava/lang/String;IIIZ)I"))
     private int draw(DrawContext instance, TextRenderer renderer, String text, int x, int y, int color, boolean shadow) {
-        try {
-            String newText = text;
+        if (!text.isEmpty()) {
             try {
-                int count = Integer.parseInt(Objects.requireNonNull(Formatting.strip(text)));
-                if (count >= 1e9)
-                    newText = Formatting.AQUA + formatDouble(count / 1000000000d, 2) + "B";
-                else if (count >= 1e6)
-                    newText = Formatting.GREEN + formatDouble(count / 1000000d, 2) + "M";
-                else if (count >= 1e3)
-                    newText = Formatting.YELLOW + formatDouble(count / 1000d, 1) + "K";
-            }
-            catch (NumberFormatException e) {
-                InventoryMod.LOGGER.warn("Failed to parse text!");
-            }
+                String newText = text;
+                try {
+                    int count = Integer.parseInt(Objects.requireNonNull(Formatting.strip(text)));
+                    if (count >= 1e9)
+                        newText = Formatting.AQUA + formatDouble(count / 1000000000d, 2) + "B";
+                    else if (count >= 1e6)
+                        newText = Formatting.GREEN + formatDouble(count / 1000000d, 2) + "M";
+                    else if (count >= 1e3)
+                        newText = Formatting.YELLOW + formatDouble(count / 1000d, 1) + "K";
+                } catch (NumberFormatException e) {
+                    InventoryMod.LOGGER.warn("Failed to parse text!");
+                }
 
-            float scale = 0.55f;
-            instance.getMatrices().scale(scale, scale, 1);
-            return instance.drawText(renderer, newText, (int) ((x + renderer.getWidth(text)) / scale - renderer.getWidth(newText) - 3), (int) (y / scale + 4), color, shadow);
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-            return instance.drawText(renderer, text, x, y, color, shadow);
-        }
+                float scale = 0.55f;
+                instance.getMatrices().scale(scale, scale, 1);
+                return instance.drawText(renderer, newText, (int) ((x + renderer.getWidth(text)) / scale - renderer.getWidth(newText) - 3), (int) (y / scale + 4), color, shadow);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return instance.drawText(renderer, text, x, y, color, shadow);
+            }
+        } else return instance.drawText(renderer, text, x, y, color, shadow);
     }
 }
